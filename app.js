eeab3847fe86f920216d54278beb548f2f7246f0
Frank Njoroge <FNjoro29@gmail.com>
2021-06-12 12:17:42 +0300

    follow up commit

--- app.js
+++ app.js
@@ -1,64 +1,60 @@
-  const cityForm = document.querySelector('form')
-  const card = document.querySelector('.card')
-  const details = document.querySelector('.details')
-  const time = document.querySelector('img.time')
-  const icon = document.querySelector('.icon img')
-  const forecast = new Forecast()
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+const forecast = new Forecast();
 
-  cityForm.addEventListener('submit', e => {
-    // prevent default action
-    e.preventDefault()
+cityForm.addEventListener("submit", (e) => {
+  // prevent default action
+  e.preventDefault();
 
-  // get city value 
-    const city = cityForm.city.value.trim()
-    cityForm.reset()
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
 
-  // update the UI with new city 
-  forecast.updateCity(city)
-      .then(data => updateUI(data))
-      .catch(err => console.log(err))
+  // update the UI with new city
+  forecast
+    .updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 
-   // set city to local storage
-      localStorage.setItem('city', city)
- })
+  // set city to local storage
+  localStorage.setItem("city", city);
+});
 
-  //function to update the UI
-  
-  const updateUI = data => {
-      console.log(data)
+//function to update the UI
 
-    // destructure properties
-    const { cityDets, weather } = data
+const updateUI = (data) => {
+  console.log(data);
 
-    // update the details template
-    details.innerHTML = `
+  // destructure properties
+  const { cityDets, weather } = data;
+
+  // update the details template
+  details.innerHTML = `
         <h5 class="my-3">${cityDets.EnglishName}</h5>
             <div class="my-3">${weather.WeatherText}</div>
             <div class="display-4 my-4">
                 <span>${weather.Temperature.Metric.Value}</span>
                 <span>&deg;C</span>
-    `
-      // remove the d-none class if present
-      if(card.classList.contains('d-none')){
-        card.classList.remove('d-none')
-    }
-
-    // update the night and day images and icons
-    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
-    icon.setAttribute('src', iconSrc)
-
-    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg' 
-    time.setAttribute('src', timeSrc)
+    `;
+  // remove the d-none class if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
   }
 
-   if(localStorage.getItem('city')){
-     forecast.updateCity(localStorage.getItem('city'))
-     .then(data => updateUI(data))
-     .catch(err =>console.log(err))
-   }
+  // update the night and day images and icons
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
 
- 
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
+};
 
-    
-
-
+if (localStorage.getItem("city")) {
+  forecast
+    .updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
